11a25529a684b1d68738bf1d17b37a4de74b298a
Theo Markettos <atm26@cl.cam.ac.uk>
2012-09-20 21:17:21 +0100

    Import Alan's recent changes from SVN, renumber to 0.7-2, push up to ROOL

--- make-all.sh
+++ make-all.sh
@@ -10,10 +10,14 @@ cd ../../packman/ &&\
 ../../../../gccsdk412/env/ro-make -j8 &&\
 ../../../../gccsdk412/env/ro-make -f RiscPkg/Rules &&\
 cp RiscPkg/PackMan_0* ../packages/autobuilder_packages/Admin/ &&\
-cp RiscPkg/PackMan_0.7-1.zip /home/atm26/public_html/ephemeral/packman07.zip && \
-cd ../packages && \
+cp RiscPkg/PackMan_0.7-2.zip /home/atm26/public_html/ephemeral/packman07.zip && \
+pushd ../packages && \
 ./build-website &&\
  ./build-index &&\
-rsync -av autobuilder_website/ tmarkettos@packages.riscosopen.org:/var/www/packages/
+popd
+#rsync -av autobuilder_website/ tmarkettos@packages.riscosopen.org:/var/www/packages/
+scp RiscPkg/PackMan_0.7-2.zip tmarkettos@packages.riscosopen.org:gccsdk412/autobuilder/autobuilder_packages/Admin/
+ssh tmarkettos@packages.riscosopen.org "cd gccsdk412/autobuilder; ./build-website; mv autobuilder_website/pkg/autobuilt autobuilder_website/pkg/raspberrypi-system; rsync -av autobuilder_website/ /var/www/packages/"
+
 
  
